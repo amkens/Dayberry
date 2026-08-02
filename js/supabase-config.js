026387640193e76d05b28cac1fc21d5af89ef09a
Sri Pranaya <sripranaya@Sris-MacBook-Air.local>
2026-08-02 17:02:33 +0530

    Add Supabase config for GitHub Pages

---
+++ js/supabase-config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL =
+    "https://dkyphuvsclfraikvimzz.supabase.co";
+
+const SUPABASE_ANON_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRreXBodXZzY2xmcmFpa3ZpbXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNjA3NzUsImV4cCI6MjEwMDczNjc3NX0.8HKON11eMkbkRIakOwU-08x9AVdI45tQ3A7GfketwzM";
